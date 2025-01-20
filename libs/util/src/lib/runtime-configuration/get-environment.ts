091f294d76fd1d62561d1b99d517b6574417cc2e
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2025-01-20 11:38:12 +0100

    feat(ci-cd): Enhance error message of getEnvironment().

--- libs/util/src/lib/runtime-configuration/get-environment.ts
+++ libs/util/src/lib/runtime-configuration/get-environment.ts
@@ -5,7 +5,6 @@ export function getEnvironment(): string {
   else if (wildcardTextMatcher(location.origin, 'http://localhost*')) return 'local';
   else if (wildcardTextMatcher(location.origin, 'http://*.elasticbeanstalk.com')) return 'aws';
   else {
-    console.log('in error');
-    throw Error('Unexpected base URL');
+    throw Error(`Undefined base URL: ${location.origin}`);
   }
 }
