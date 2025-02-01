bb3024b488168a34ab0822c295942165104e45c5
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2025-02-01 15:11:29 +0100

    feat(e2e test with Playwright): configure e2e tests in CI.

--- apps/processpuzzle-testbed-e2e/playwright.config.ts
+++ apps/processpuzzle-testbed-e2e/playwright.config.ts
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv';
 import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
 
 import { workspaceRoot } from '@nx/devkit';
 
+dotenv.config({ path: `./env/.env.${process.env['ENVIRONMENT']}` });
+
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
+const baseURL = process.env['PROCESSPUZZLE_TESTBED_BASE_URL'] || 'http://localhost:4200';
 
 /**
  * Read environment variables from file.
@@ -17,6 +20,8 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
  */
 export default defineConfig({
   ...nxE2EPreset(__filename, { testDir: './src' }),
+  // Opt out of parallel tests on CI.
+  workers: process.env['ENVIRONMENT'] === 'ci' ? 1 : undefined,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
