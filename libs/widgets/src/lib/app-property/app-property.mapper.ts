aca0ae8dfcc0bbb5e9737dc8a1ed1082b6f9e54d
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2025-04-25 09:57:03 +0200

    fix(Widgets): Correct Sonar issues.

--- libs/widgets/src/lib/app-property/app-property.mapper.ts
+++ libs/widgets/src/lib/app-property/app-property.mapper.ts
@@ -2,11 +2,13 @@ import { ApplicationProperty } from './app-property';
 import { BaseEntityMapper } from '@processpuzzle/base-entity';
 
 export class ApplicationPropertyMapper implements BaseEntityMapper<ApplicationProperty> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   fromDto(dto: any): ApplicationProperty {
     return new ApplicationProperty(dto.name, dto.value);
   }
 
-  toDto(entity: ApplicationProperty) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  toDto(entity: ApplicationProperty): any {
     return { name: entity.name, value: entity.value };
   }
 }
