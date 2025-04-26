bdbaaa3055ad23868c53ab65b94f44f9745e946f
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2025-04-26 11:24:30 +0200

    fix(LanguageSelector): Delete unnecessary transloco config.

--- libs/widgets/src/lib/widgets.module.ts
+++ libs/widgets/src/lib/widgets.module.ts
@@ -1,9 +1,5 @@
-import { provideTransloco, TranslocoModule, TranslocoService } from '@jsverse/transloco';
+import { provideTransloco, TranslocoModule } from '@jsverse/transloco';
 import { inject, isDevMode, NgModule } from '@angular/core';
-import de from './assets/i18n/widgets/de.json';
-import hu from './assets/i18n/widgets/hu.json';
-import en from './assets/i18n/widgets/en.json';
-import es from './assets/i18n/widgets/es.json';
 import { TranslocoHttpLoader } from './language-selector/transloco.loader';
 import { LanguageConfig } from './language-selector/language-config';
 import { RUNTIME_CONFIGURATION } from '@processpuzzle/util';
@@ -39,11 +35,4 @@ import { RUNTIME_CONFIGURATION } from '@processpuzzle/util';
     }),
   ],
 })
-export class WidgetsModule {
-  constructor(private _translate: TranslocoService) {
-    this._translate.setTranslation({ widgets: de }, 'de');
-    this._translate.setTranslation({ widgets: hu }, 'hu');
-    this._translate.setTranslation({ widgets: en }, 'en');
-    this._translate.setTranslation({ widgets: es }, 'es');
-  }
-}
+export class WidgetsModule {}
