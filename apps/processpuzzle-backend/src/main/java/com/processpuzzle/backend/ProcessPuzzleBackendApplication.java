c58a1e923310fa528b89450cb94594a8e49a3013
ZsZs <zsolt.zsuffa@gmail.com>
2026-03-26 11:25:46 +0100

    feature(processpuzzle-backend): Empty Spring Boot applikation.

---
+++ apps/processpuzzle-backend/src/main/java/com/processpuzzle/backend/ProcessPuzzleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.processpuzzle.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessPuzzleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessPuzzleBackendApplication.class, args);
+    }
+
+}
