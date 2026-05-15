0b43f4da3d1248d26c3b51f00bc958bf43bdbad1
ZsZs <zsolt.zsuffa@gmail.com>
2026-05-15 11:20:19 +0200

    fix(base-entity): Add data-testid attribute to generated form controls.

--- libs/base-entity/src/lib/base-entity-facade/base-entity-facade-registry.ts
+++ libs/base-entity/src/lib/base-entity-facade/base-entity-facade-registry.ts
@@ -1,4 +1,7 @@
-import { Component, inject, InjectionToken, OnInit, type ProviderToken } from '@angular/core';
+import { Component, computed, inject, Injector, InjectionToken, type ProviderToken, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { BaseEntityAttrDescriptor } from '../base-entity/base-entity-attr.descriptor';
 import { BaseEntityFacade } from './base-entity-facade';
 
 export type BaseEntityFacadeRegistry = Record<string, ProviderToken<BaseEntityFacade<any>>>;
@@ -9,12 +12,56 @@ export const BASE_ENTITY_FACADE_REGISTRY = new InjectionToken<BaseEntityFacadeRe
 
 @Component({
   selector: 'pp-entity-registry',
-  template: '',
+  template: '<pre>{{ json() }}</pre>',
 })
-export class EntityRegistryComponent implements OnInit {
+export class EntityRegistryComponent {
   private registry = inject(BASE_ENTITY_FACADE_REGISTRY);
+  private injector = inject(Injector);
+  private queryParams = toSignal(inject(ActivatedRoute).queryParamMap, { initialValue: null });
 
-  ngOnInit() {
-    // expose on window for Playwright to read
+  private descriptors = signal(this.buildDescriptors());
+
+  protected json = computed(() => {
+    const minified = this.queryParams()?.get('minified') === 'yes';
+    return minified ? JSON.stringify(this.descriptors()) : JSON.stringify(this.descriptors(), null, 2);
+  });
+
+  private buildDescriptors() {
+    return Object.entries(this.registry).map(([, facadeToken]) => {
+      const facade = this.injector.get(facadeToken);
+      const descriptor = facade.descriptor;
+      return {
+        entityName: descriptor.entityName,
+        entityTitle: descriptor.entityTitle,
+        attrDescriptors: descriptor.attrDescriptors.map((attr) => this.serializeAttr(attr)),
+      };
+    });
+  }
+
+  private serializeAttr(attr: any) {
+    const isBaseAttr = attr instanceof BaseEntityAttrDescriptor;
+    return {
+      attrName: attr.attrName,
+      formControlType: attr.formControlType,
+      disabled: attr.disabled,
+      style: attr.style,
+      ...(isBaseAttr && {
+        label: attr.label,
+        description: attr.description,
+        styleClass: attr.styleClass,
+        labelClass: attr.labelClass,
+        format: attr.format,
+        isLinkToDetails: attr.isLinkToDetails,
+        selectables: attr.selectables,
+        visible: attr.visible,
+        hideInTable: attr.hideInTable,
+        isHeading: attr.isHeading,
+        placeholder: attr.placeholder,
+        lines: attr.lines,
+        options: attr.options,
+        required: attr.required,
+        linkedEntityType: attr.linkedEntityType ? { entityName: attr.linkedEntityType.entityName } : undefined,
+      }),
+    };
   }
 }
--- libs/base-entity/src/lib/base-form/base-form-control.component.ts
+++ libs/base-entity/src/lib/base-form/base-form-control.component.ts
@@ -1,9 +1,10 @@
-import { Component, computed, inject, Injector, input, InputSignal } from '@angular/core';
+import { Component, computed, HostBinding, inject, Injector, input, InputSignal } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { NGXLogger } from 'ngx-logging-kit';
 import { BaseEntity } from '../base-entity/base-entity';
 import { BaseEntityAttrDescriptor } from '../base-entity/base-entity-attr.descriptor';
 import { BaseEntityDescriptor } from '../base-entity/base-entity.descriptor';
+import { entityNameFromType } from '../base-entity-store/base-entity.store';
 import { BaseFormNavigatorSingletonStore } from '../base-form-navigator/base-form-navigator.store';
 import { BASE_ENTITY_FACADE_REGISTRY } from '../base-entity-facade/base-entity-facade-registry';
 
@@ -27,6 +28,16 @@ export abstract class BaseFormControlComponent<Entity extends BaseEntity> {
     return linkedEntityType;
   });
   value: InputSignal<any> = input.required();
+
+  @HostBinding('attr.data-testid')
+  get testId(): string | null {
+    const attrName = this.config()?.attrName;
+    const entity = this.entity();
+    if (!attrName || !entity) return null;
+    const entityName = entityNameFromType(entity.constructor as new () => Entity);
+    return `${entityName}-${attrName}`;
+  }
+
   protected readonly logger = inject(NGXLogger);
   protected readonly formNavigator = inject(BaseFormNavigatorSingletonStore);
   private readonly injector = inject(Injector);
