8ced2bc506ef01c31bea189a940306c79f506971
ZsZs <zsolt.zsuffa@gmail.com>
2026-06-12 18:16:44 +0200

    fix(util): Adjust vitest configuration.

--- libs/js-shared/util/vitest.config.ts
+++ libs/js-shared/util/vitest.config.ts
@@ -4,10 +4,19 @@ import { dirname, resolve } from 'node:path';
 
 const projectRoot = dirname(fileURLToPath(import.meta.url));
 
+const projectPrefix = 'libs/js-shared/util';
+
 export default defineConfig({
   test: {
     coverage: {
       reportsDirectory: resolve(projectRoot, 'reports/coverage'),
+      include: [`${projectPrefix}/src/**/*.ts`],
+      exclude: [
+        `${projectPrefix}/src/public-api.ts`,
+        `${projectPrefix}/src/test-setup.ts`,
+        `${projectPrefix}/src/**/*.module.ts`,
+        `${projectPrefix}/src/**/*.spec.ts`,
+      ],
     },
   },
 });
