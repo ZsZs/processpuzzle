602ec88174eca6429460a30412a29da0989f145d
ZsZs <zsolt.zsuffa@gmail.com>
2026-06-25 21:26:11 +0200

    feature(processpuzzle-backend): Integrate base-rule into backend.

--- apps/processpuzzle-backend/src/main/java/com/processpuzzle/backend/ProcessPuzzleBackendApplication.java
+++ apps/processpuzzle-backend/src/main/java/com/processpuzzle/backend/ProcessPuzzleBackendApplication.java
@@ -3,9 +3,13 @@ package com.processpuzzle.backend;
 import com.processpuzzle.objectstore.adapters.outbound.MinioConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.processpuzzle")
+@EnableJpaRepositories(basePackages = "com.processpuzzle")
+@EntityScan(basePackages = "com.processpuzzle")
 @Import({MinioConfig.class})
 public class ProcessPuzzleBackendApplication {
 
