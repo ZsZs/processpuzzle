a5677f8423ec51412414a6a27d73ebae3c483964
ZsZs <zsolt.zsuffa@gmail.com>
2026-08-09 12:35:41 +0200

    feature(tools-firebase): Turn Firebase Functions into it's own Nx project.

---
+++ tools/firebase/functions/vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+import { join } from 'node:path';
+
+export default defineConfig({
+  root: __dirname,
+  test: {
+    environment: 'node',
+    include: ['src/**/*.spec.ts'],
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reportsDirectory: join(__dirname, 'reports/coverage'),
+      reporter: ['lcov', 'html', 'text'],
+      cleanOnRerun: true,
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.spec.ts'],
+    },
+  },
+});
